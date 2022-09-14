1175feaa9fbc4e116d73b27ca92a55dc3cfa26ed
Jeferson Beiertorf <jefersonbeiertorf@gmail.com>
2022-09-13 21:31:00 -0300

    add folders

---
+++ labefood/src/pages/adressRegistration/index.js
@@ -0,0 +1,5 @@
+export const AdressRegistrationPage = () =>{
+    return(
+        <h1>Adress Registration Page</h1>
+    )
+}
---
+++ labefood/src/routes/coordinator.js
@@ -0,0 +1,19 @@
+// export const goTo = (navigator) =>{
+//     navigator('/')
+// }
+
+// export const goTo = (navigator) =>{
+//     navigator('/')
+// }
+
+// export const goTo = (navigator) =>{
+//     navigator('/')
+// }
+
+// export const goTo = (navigator) =>{
+//     navigator(`/`)
+// }
+
+// export const goTo = (navigator) =>{
+//     navigator('/')
+// }
