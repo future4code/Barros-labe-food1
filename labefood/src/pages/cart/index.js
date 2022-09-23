ff5967daac1dfcf406366336566d784f1a64b623
Leonardo Barros de Souza <leonardo.bsouza@hotmail.com>
2022-09-23 10:06:02 -0300

    feat: renderizar de forma diferente se existir algo no carrinho

--- labefood/src/pages/cart/index.js
+++ labefood/src/pages/cart/index.js
@@ -1,50 +1,102 @@
 import { CartContainer } from "./style";
 import { Stack, Radio, RadioGroup, Button } from "@chakra-ui/react";
+import { useState } from "react";
 
 export const CartPage = () => {
-  return (
-    <CartContainer>
-      <header>
-        <h1>Meu carrinho</h1>
-      </header>
-      <div className="address">
-        <p>Endereço de entrega</p>
-        <p>Rua teste, 13</p>
-      </div>
-      <div className="title">
-        <h2>Carrinho vazio</h2>
-      </div>
-      <div className="price-container">
-        <div className="subtotal">
-          <p>SUBTOTAL</p>
+  const [cart, setCart] = useState(false);
+
+  if (cart) {
+    return (
+      <CartContainer>
+        <header>
+          <h1>Meu carrinho</h1>
+        </header>
+
+        <div className="address">
+          <p>Endereço de entrega</p>
+          <p>Rua teste, 13</p>
         </div>
-        <div className="price">
-          <p>Frete R$0,00</p>
-          <p>R$00,00</p>
+        <div className="title">
+          <h2>Carrinho vazio</h2>
         </div>
-      </div>
-      <div className="payment-title">
-        <p>Forma de pagamento</p>
-      </div>
-      <span className="line"></span>
-      
-      <RadioGroup width="85vw">
-        <Stack direction="column">
-          <Radio value="1">Dinheiro</Radio>
-          <Radio value="2">Cartão de crédito</Radio>
-        </Stack>
-      </RadioGroup>
-      <div className="submit-button">
-        <Button
-          type="submit"
-          colorScheme="red"
-          variant="solid"
-          borderRadius="2px"
-          height="2.625rem"
-        >
-          Entrar
-        </Button>
-      </div>
-    </CartContainer>
-  );
+        <div className="price-container">
+          <div className="subtotal">
+            <p>SUBTOTAL</p>
+          </div>
+          <div className="price">
+            <p>Frete R$0,00</p>
+            <p>R$00,00</p>
+          </div>
+        </div>
+        <div className="payment-title">
+          <p>Forma de pagamento</p>
+        </div>
+        <span className="line"></span>
+
+        <RadioGroup width="85vw">
+          <Stack direction="column">
+            <Radio value="1">Dinheiro</Radio>
+            <Radio value="2">Cartão de crédito</Radio>
+          </Stack>
+        </RadioGroup>
+        <div className="submit-button">
+          <Button
+            type="submit"
+            colorScheme="red"
+            variant="solid"
+            borderRadius="2px"
+            height="2.625rem"
+          >
+            Confirmar
+          </Button>
+        </div>
+      </CartContainer>
+    );
+  } else {
+    return (
+      <CartContainer>
+        <header>
+          <h1>Meu carrinho</h1>
+        </header>
+        <div className="address">
+          <p>Endereço de entrega</p>
+          <p>Rua teste, 13</p>
+        </div>
+        <div className="title">
+          <h2>Carrinho cheio</h2>
+        </div>
+        <div className="price-container">
+          <div className="subtotal">
+            <p>SUBTOTAL</p>
+          </div>
+          <div className="price">
+            <p>Frete R$0,00</p>
+            <p>R$00,00</p>
+          </div>
+        </div>
+        <div className="payment-title">
+          <p>Forma de pagamento</p>
+        </div>
+        <span className="line"></span>
+
+        <RadioGroup width="85vw">
+          <Stack direction="column">
+            <Radio value="1">Dinheiro</Radio>
+            <Radio value="2">Cartão de crédito</Radio>
+          </Stack>
+        </RadioGroup>
+        <div className="submit-button">
+          <Button
+            type="submit"
+            colorScheme="red"
+            variant="solid"
+            borderRadius="2px"
+            height="2.625rem"
+          >
+            Confirmar
+          </Button>
+        </div>
+      </CartContainer>
+    );
+  }
 };
