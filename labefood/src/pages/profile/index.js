95d8ab77d9595fcf4b146f7088681f0fb8c3abc9
Luana Barbosa de Oliveira <deoliveiraluana29@gmail.com>
2022-09-26 23:10:00 -0300

    profile

--- labefood/src/pages/profile/index.js
+++ labefood/src/pages/profile/index.js
@@ -1,20 +1,22 @@
+import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { Footer } from "../../components/footer/Footer";
 import { useProtectPage } from "../../hooks/useProtectPage";
 import {useRequestData} from "../../hooks/useRequestData"
 import {Header } from "../../components/header";
+import { BASE_URL } from "../../constants/index";
 
 export const ProfilePage = () => {
   useProtectPage();
   const navigate = useNavigate();
-  // const [profile] = useRequestData(`/{appName}/profile`, {});
+  
 
   return (
   
   
   <>
   <p>Meu Perfil</p>
-  
+  <Header showArrow={'false'} showTitle={'true'} title={"Meu perfil"} />
   <Footer/>
   </>
   
