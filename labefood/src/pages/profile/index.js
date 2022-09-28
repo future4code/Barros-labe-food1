0ce05a47a4b65b4a93a411eaf3f6c017d06716c8
Jeferson Beiertorf <jefersonbeiertorf@gmail.com>
2022-09-27 22:49:20 -0300

    update profile

--- labefood/src/pages/profile/index.js
+++ labefood/src/pages/profile/index.js
@@ -4,50 +4,72 @@ import { useNavigate } from "react-router-dom";
 import { Footer } from "../../components/footer/Footer";
 import { appName, BASE_URL, updateProfile } from "../../constants";
 import { useProtectPage } from "../../hooks/useProtectPage";
-import { ProfileContainer, UserInfo, UserRequests} from "./style";
+import { ProfileContainer, UserAddress, UserInfo, UserRequests, userInfoContainer} from "./style";
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
+import { MdEdit } from "react-icons/md";
+
+
+
 
 export const ProfilePage = () => {
   useProtectPage();
   const navigate = useNavigate();
   const [profiles, setProfiles] = useState({})
-
+  
   const token = localStorage.getItem("token")
+  
 
 
-  const userProfile = () => {
-      axios.get(`${BASE_URL}/${appName}/profile`, {
-      headers: { auth: token }
+const getProfile = () =>{
+  axios.get(`${BASE_URL}/${appName}/profile`,
+    {
+      headers:{
+        auth: token
+      }
     }).then((response)=>{
-      response.json().then(function(conversion){
-        console.log(conversion.user)
-      })})
-    .catch((erro) => { console.log(erro) })
+      setProfiles(response.data.user)
+      console.log(response);
+    }).catch((e)=>{
+      console.log(e);
+    })
   }
-  console.log(userProfile);
-    // useEffect(() => { userProfile() }, [])
-
-
-      return (
+  useEffect(() => { getProfile() }, [])
+  
+  
+  
+  
+  return (
     
         <ProfileContainer>
           <Stack padding="5" alignItems="center">
               <h2>Meu perfil</h2>
-        <Divider padding="5px"/>
           </Stack>
-        <UserInfo>
-          <div className="userInfo">
-    
-          </div>
-          <Stack bg="blackAlpha.100" alignItems="flex-start" width="100%" padding="10px"  >
+        <Divider/>
+
+          <UserInfo>
+            <section>
+              <h1>{profiles && profiles.name}</h1>
+              <h3>{profiles && profiles.email}</h3>
+              <h3>{profiles && profiles.cpf}</h3>
+            </section>
+            <button>
+              <MdEdit />
+            </button>
+          </UserInfo>
+        
+        <UserAddress>
+          <div>
             <p>Endereço cadastrado</p>
-          </Stack>
-      
-        </UserInfo>
+            <h3>{profiles && profiles.address}</h3>
+          </div>
+            <button>
+              <MdEdit />
+            </button>
+        </UserAddress>
         <UserRequests>
-        <h2>Histórico de pedidos</h2>
+          <h2>Histórico de pedidos</h2>
         <Divider width="90vw" borderColor="black" />
         </UserRequests>
         <Footer />
@@ -55,54 +77,8 @@ export const ProfilePage = () => {
 
 )
 };
-//   const [form, onChangeInputs] = useForm({
-//     name:"",
-//     email: "",
-//     cpf:""
-    
-// });
-// //--- Estados com valores booleanos para a validação do dados preenchidos no form
-// const [isNameValid, setIsNameValid] = useState(true)
-// const [isEmailValid, setIsEmailValid] = useState(true)
-// const [isCpfValid, setIsCpfValid] = useState(true)
 
-// const onSubmit =  (e)=>{
-//   e.preventDefault();
-//     //--- Testes de validação que retorna booleanos para usar nas mensagens de erro do form control...
-//   setIsNameValid(validateInput(form.name))
-//   setIsEmailValid(validateInput(form.email))
-//   setIsCpfValid(validateInput(form.cpf))
 
-  
-//   try{
-//       if(isNameValid && isEmailValid && isCpfValid ){
-//           updateProfile({
-//             name: form.name,
-//             email: form.email,
-//             cpf: form.cpf
-//           })
-//           alert("Cadastro efetuado com sucesso!")
-//           }
-//       } catch(e){
-//           console.log(e.response);
-//       }
-//   }
-  
-
-  // const updateProfile =  fetch(
-  //   `${BASE_URL}/${appName}/profile`,
-  //   {
-  //       headers:{
-  //         auth: token
-  //       }
-  //   }
-  // ).then((response)=>{
-  //   response.json().then(function(conversion){
-  //     console.log(conversion.user);
-  //   })
-  // })
-  // .catch((e)=>{console.log(e);})
-  //   console.log(updateProfile);
 
 
 
--- labefood/src/pages/profile/style.js
+++ labefood/src/pages/profile/style.js
@@ -1,3 +1,4 @@
+import styledEngine from "@mui/styled-engine"
 import styled from "styled-components"
 
 
@@ -8,15 +9,21 @@ export const ProfileContainer = styled.main`
     flex-direction: column;
     min-height: 100vh;
     padding: 0 0 1.625rem;
+    h1, h2, h3{
+        font-family: "Roboto", sans-serif;
+    }
     `
-export const UserInfo = styled.div`
-display:flex;
-
-
+    // bg="blackAlpha.100" alignItems="flex-start" width="100%" padding="10px"
+export const UserAddress = styled.div`
+    background-color: #eee;
+    display:flex;
+    justify-content:space-between;
+    padding:10px;
+    
 p{
-    color: #b8b8b8
+    color: #b8b8b8;
+    padding: 10px 0
 }
-
 
 `
 export const UserRequests = styled.div`
@@ -24,4 +31,15 @@ export const UserRequests = styled.div`
     flex-direction:column;
     align-items: center;
     padding:10px;
+
+`
+
+
+export const UserInfo = styled.div`
+    display:flex;
+    padding:10px;
+    justify-content: space-between;
+    button{
+        margin-bottom:40px;
+    }
 `
