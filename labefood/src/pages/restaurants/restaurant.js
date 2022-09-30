81b5c81e70b8af5342dee9cf8754507590bbeb60
Leonardo Barros de Souza <leonardo.bsouza@hotmail.com>
2022-09-30 11:01:22 -0300

    fix: importar useContext

--- labefood/src/pages/restaurants/restaurant.js
+++ labefood/src/pages/restaurants/restaurant.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { appName, BASE_URL } from "../../constants";
 import { useProtectPage } from "../../hooks/useProtectPage";
